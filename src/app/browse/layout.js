a1513ea5cd64b0c627ab6fc668eb7055659af956
Sadiqunnabi17 <sadiqunnabi@gmail.com>
2026-06-20 10:35:21 +0600

    Added SEO metadata to browse page

---
+++ src/app/browse/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Browse Ebooks - Fable",
+  description: "Discover and purchase original ebooks from talented writers",
+};
+
+export default function BrowseLayout({ children }) {
+  return children;
+}
